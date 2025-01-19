d42347b2d1d82243b4c2be295703a178a37e56cf
Shaik Asif <bhanurshaikasif@gmail.com>
2025-01-19 16:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// Get references to the dropdown and button
+const colorSelect = document.getElementById('colorSelect');
+const removeButton = document.querySelector('input[type="button"]');
+
+// Add a click event listener to the button
+removeButton.addEventListener('click', () => {
+    // Get the selected option's index
+    const selectedIndex = colorSelect.selectedIndex;
+    if (selectedIndex !== -1) {
+        // Remove the selected option from the dropdown
+        colorSelect.remove(selectedIndex);
+    } else {
+        alert('Please select a color to remove.');
+    }
+});
